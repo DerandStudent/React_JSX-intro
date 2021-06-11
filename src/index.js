f686779bde999184c64d99d792d4553d3917343f
DerandStudent <55449689+DerandStudent@users.noreply.github.com>
2021-06-11 15:45:23 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+//var React = require("react");
+import ReactDOM from "react-dom";
+//var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>hello</h1>,
+  </div>,
+  document.getElementById("root")
+);
